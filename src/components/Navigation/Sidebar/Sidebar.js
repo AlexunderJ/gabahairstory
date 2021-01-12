0cb4564d0f993bb5310b21b18e67637b56485a22
AlexunderJ <pozdrower@wp.pl>
2021-01-12 21:34:31 +0100

    sass for home and sidebar

--- src/components/Navigation/Sidebar/Sidebar.js
+++ src/components/Navigation/Sidebar/Sidebar.js
@@ -8,6 +8,7 @@ export default function Sidebar(props) {
      return(
 <ul onClick={props.clicked} className={'NavigationItem'}>
     <NavigationItem link='/' exact >Home</NavigationItem>
+    <li><a href='https://sklep.gabahairstory.pl/'>Sklep</a></li>
     <NavigationItem link='/team'>Team</NavigationItem>
     <NavigationItem link='/cennik'>Cennik</NavigationItem>
     <NavigationItem link='/kontakt'>Kontakt</NavigationItem>
